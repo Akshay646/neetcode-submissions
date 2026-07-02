75b9456da2aa7a54d3316d715d948b66c2dcee81
Akshay Yadav <ady23617@gmail.com>
2026-07-02 09:14:16 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void backtrack(int openP, int closeP, string path, vector<string>& ans,
+    int n){
+        //we have got one of the valid permutation of parenthesis
+        if(openP == n && closeP == n){
+            ans.push_back(path);
+            return;
+        }
+
+        //Validation check 1 for add open parenthesis
+        if(openP < n){
+            path.push_back('(');
+            //used one open parenthesis, so openP + 1, go to next possible valid
+            //parenthesis
+            backtrack(openP + 1, closeP, path, ans, n);
+            //backtrack, as when returning from down, removed already added brackets
+            path.pop_back();
+        }
+
+        //Validation check 2 for adding closing parenthesis
+        if(closeP < openP){
+            path.push_back(')');
+            backtrack(openP, closeP + 1, path, ans, n);
+            path.pop_back();
+        }
+
+        //above both conditions make sure we add only valid parentheis in the path
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string path;
+        backtrack(0, 0, path, ans, n);
+        return ans;
+    }
+};
