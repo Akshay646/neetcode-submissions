d5a8229beabdfca5b88609978216ed1a8f1595ba
Akshay Yadav <ady23617@gmail.com>
2026-07-28 09:49:34 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        queue<pair<int,int>> q;
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> seen(m, vector<bool>(n, false));
+        //Add all '0' cells apart from the cells which are at boundaries
+        for(int r = 0; r < m; r++){
+            for(int c = 0; c < n; c++){
+                //this will help not add cells which are '1' & at boundaries and 
+                if (board[r][c] == 'O' &&
+                (r == 0 || c == 0 || r == m - 1 || c == n - 1))
+                {
+                    q.push({r, c});
+                    seen[r][c] = true;
+                }
+            }
+        }
+       
+        //Using BFS, just mark all the cells True whichever are reachable from these
+        //cells on the edge
+        while(!q.empty()){
+            auto[r, c] = q.front();
+            q.pop();
+
+            int dR[4] = {0, 1, 0, -1};
+            int dC[4] = {1, 0, -1, 0};
+
+            for(int i = 0; i < 4; i++){
+                int nR = r + dR[i];
+                int nC = c + dC[i];
+
+                if(nR >= 0 && nC >= 0 && nR < m && nC < n && !seen[nR][nC] && board[nR][nC] == 'O'){
+                    q.push({nR, nC});
+                    seen[nR][nC] = true;
+                }
+            }
+        }
+        
+         for(int r = 0; r < m; r++){
+            for(int c = 0; c < n; c++){
+                if(board[r][c] == 'O' && !seen[r][c]){
+                    board[r][c] = 'X';
+                }
+            }
+         }
+    }
+};
