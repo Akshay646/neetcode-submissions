3044a5fdcc5bebc8fb163e68f03f38f4afc937af
Akshay Yadav <ady23617@gmail.com>
2026-08-04 20:47:52 +0530

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int evaluateExp(int a, int b, string& op){
+        switch(op[0]){
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '/':
+                return a / b;
+            case '*':
+                return a * b;
+            default:
+                return 0;
+        }
+    }
+    bool IsOperator(string s){
+        return s == "*" || s == "+" || s == "/" || s == "-";
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i = 0; i < tokens.size(); i++){
+            if(IsOperator(tokens[i])){
+                int second = st.top();
+                st.pop();
+                int first = st.top();
+                st.pop();
+                int eval = evaluateExp(first, second, tokens[i]);
+                //now push back to stack again as it will be one of the
+                //operands for next evaluation as shown in problem
+                //statement
+                st.push(eval);
+                continue;
+            }
+            st.push(stoi(tokens[i]));
+        }
+
+        return st.empty() ? 0 : st.top();
+
+    }
+};
